065b2b86f4ff46aa770224c9351670b0f8b3e54e
Practics <sainig273@gmail.com>
2025-10-07 13:48:46 +0530

    first commit

---
+++ auth-service/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Auth-Service running on port ${PORT}`));
